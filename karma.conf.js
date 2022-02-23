cbf7d3dee96f30d751ef1399a0c1bf000de24894
zerostar <joseinformatico2015@gmail.com>
2022-02-23 18:31:45 -0300

    change pipeline

--- karma.conf.js
+++ karma.conf.js
@@ -1,87 +1,46 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// module.exports = function (config) {
-//   config.set({
-//     basePath: '',
-//     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-//     plugins: [
-//       require('karma-jasmine'),
-//       require('karma-chrome-launcher'),
-//       require('karma-jasmine-html-reporter'),
-//       require('karma-coverage'),
-//       require('@angular-devkit/build-angular/plugins/karma')
-//     ],
-//     client: {
-//       jasmine: {
-//         // you can add configuration options for Jasmine here
-//         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-//         // for example, you can disable the random execution with `random: false`
-//         // or set a specific seed with `seed: 4321`
-//       },
-//       clearContext: false // leave Jasmine Spec Runner output visible in browser
-//     },
-//     jasmineHtmlReporter: {
-//       suppressAll: true // removes the duplicated traces
-//     },
-//     coverageReporter: {
-//       dir: require('path').join(__dirname, './coverage/angular-api'),
-//       subdir: '.',
-//       reporters: [
-//         { type: 'html' },
-//         { type: 'text-summary' }
-//       ]
-//     },
-//     reporters: ['progress', 'kjhtml'],
-//     port: 9876,
-//     colors: true,
-//     logLevel: config.LOG_INFO,
-//     autoWatch: true,
-//     browsers: ['Chrome'],
-//     singleRun: false,
-//     restartOnFileChange: true
-//   });
-// };
-
-const os = require('os');
-const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;module.exports = function (config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-    reporters: ['mocha','coverage'],
     coverageReporter: {
-      type: 'text-summary'
+      dir: require('path').join(__dirname, './coverage/angular-api'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browserNoActivityTimeout: 30000,
-    browsers: [
-      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
-    ],
-    customLaunchers: {
-      ChromeHeadless: {
-          base: 'Chrome',
-          flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
-      }
-    },
-    singleRun: true
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    restartOnFileChange: true
   });
 };
+
